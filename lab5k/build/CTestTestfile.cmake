4f184762ad1d60c439ec72cb3e371c0e1a8e985f
Kasaneru <60545303+Kasaneru@users.noreply.github.com>
2024-02-20 19:46:21 +0300

    Delete lab5k directory

--- lab5k/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/taida/Programming/OOP_Labs/lab5
-# Build directory: /home/taida/Programming/OOP_Labs/lab5/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(BASIC "/home/taida/Programming/OOP_Labs/lab5/build/tests")
-set_tests_properties(BASIC PROPERTIES  _BACKTRACE_TRIPLES "/home/taida/Programming/OOP_Labs/lab5/CMakeLists.txt;14;add_test;/home/taida/Programming/OOP_Labs/lab5/CMakeLists.txt;0;")
